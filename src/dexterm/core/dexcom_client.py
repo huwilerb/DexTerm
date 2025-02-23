203c1f1490a69d3c190e4ee86cf4da315e8e5023
huwilerb <github@blaisehuwiler.ch>
2025-02-23 14:51:29 +0100

    feat: handle dotenv files

--- src/dexterm/core/dexcom_client.py
+++ src/dexterm/core/dexcom_client.py
@@ -2,13 +2,27 @@ import os
 from pydexcom import Dexcom, Region
 from pathlib import Path
 from typing import Optional
+from dotenv import load_dotenv
 
 
 class DexcomClient:
     def __init__(self, env_file: Optional[Path] = None) -> None:
-        username = os.getenv("DEXCOM_USERNAME")  # TODO: Handle None values
-        password = os.getenv("DEXCOM_PASSWORD")  # TODO: Handle None values
-        self.dexcom = Dexcom(username=username, password=password, region=Region.OUS)
+        if env_file:
+            load_dotenv(env_file)
+
+        username = os.getenv("DEXCOM_USERNAME")
+
+        password = os.getenv("DEXCOM_PASSWORD")
+
+        if username is None:
+            msg = "No dexcom username found"
+            raise ValueError(msg)
+        if password is None:
+            msg = "No dexcom password found"
+            raise ValueError(msg)
+
+        self.dexcom = Dexcom(
+            username=username, password=password, region=Region.OUS)
 
     def get_glucose_level(self):
         return self.dexcom.get_current_glucose_reading()
